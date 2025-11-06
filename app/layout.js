211f4d6c720c4674f15dfc42463cda3ff7659fa2
Danproc <hello@thenorthern-web.co.uk>
2025-11-06 16:51:17 +0400

    Added Analytics & Search Console

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,37 @@ import { getSEOTags } from "@/libs/seo";
 import ClientLayout from "@/components/LayoutClient";
 import config from "@/config";
 import "./globals.css";
+import Script from 'next/script';
+
+  export default function RootLayout({ children }) {
+    return (
+      <html lang="en">
+        <head>
+          {/* Google Analytics */}
+          {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+            <>
+              <Script
+                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+                strategy="afterInteractive"
+              />
+              <Script id="google-analytics" strategy="afterInteractive">
+                {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+                `}
+              </Script>
+            </>
+          )}
+        </head>
+        <body>
+          <ClientLayout>{children}</ClientLayout>
+        </body>
+      </html>
+    );
+  }
+
 
 export const viewport = {
 	// Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
