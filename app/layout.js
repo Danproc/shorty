b6b638d57a22c164ff5246ebc6ff7f13fa4d2c30
Danproc <hello@thenorthern-web.co.uk>
2025-11-06 16:57:23 +0400

    fix: Remove duplicate export default in layout.js

--- app/layout.js
+++ app/layout.js
@@ -4,36 +4,6 @@ import config from "@/config";
 import "./globals.css";
 import Script from 'next/script';
 
-  export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <head>
-          {/* Google Analytics */}
-          {process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
-            <>
-              <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
-                strategy="afterInteractive"
-              />
-              <Script id="google-analytics" strategy="afterInteractive">
-                {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
-                `}
-              </Script>
-            </>
-          )}
-        </head>
-        <body>
-          <ClientLayout>{children}</ClientLayout>
-        </body>
-      </html>
-    );
-  }
-
-
 export const viewport = {
 	// Will use the primary color of your theme to show a nice theme color in the URL bar of supported browsers
 	themeColor: config.colors.main,
@@ -51,6 +21,25 @@ export default function RootLayout({ children }) {
 			lang="en"
 			style={{ fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif' }}
 		>
+			<head>
+				{/* Google Analytics */}
+				{process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID && (
+					<>
+						<Script
+							src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}`}
+							strategy="afterInteractive"
+						/>
+						<Script id="google-analytics" strategy="afterInteractive">
+							{`
+								window.dataLayer = window.dataLayer || [];
+								function gtag(){dataLayer.push(arguments);}
+								gtag('js', new Date());
+								gtag('config', '${process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID}');
+							`}
+						</Script>
+					</>
+				)}
+			</head>
 			<body>
 				{/* ClientLayout contains all the client wrappers (Crisp chat support, toast messages, tooltips, etc.) */}
 				<ClientLayout>{children}</ClientLayout>
